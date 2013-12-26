54cd63c8e5c9f3413caa1602aa29180e4b2e2af4
David Montoya <masda70@gmail.com>
2013-12-26 14:55:28 +0100

    Added empty-reply Button

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,12 @@
 # version: 0.1
 # authors: David Montoya, Charles-Pierre Astolfi
 
+# Indeed Button
 register_asset "javascripts/indeed_button_controller.js"
 register_asset "javascripts/indeed_button_view.js"
 register_asset "javascripts/indeed_button_bootstrap.js"
 register_asset "stylesheets/indeed_button.scss"
+
+# Empty Reply Button
+register_asset "javascripts/empty_reply_button.js"
+register_asset "stylesheets/empty_reply_button.scss"
