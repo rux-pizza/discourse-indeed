51180835271ab2748998e1e9da5953fc1d520182
masda70 <masda70@gmail.com>
2013-12-26 10:22:56 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: Indeed this!
 # about: Indeed up anything.
 # version: 0.1
-# authors: David Montoya, Charles-Pierre Astolfi
+# authors: David Montoya
 
 register_asset "javascripts/indeed_button_controller.js"
 register_asset "javascripts/indeed_button_view.js"
